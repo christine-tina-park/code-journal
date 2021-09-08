dbe3ababe89929b1780515e943580598e5796c32
Christine Tina Park <escp1004jk@gmail.com>
2021-09-08 11:54:56 -0700

    function handleSubmit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 /* global data */
 /* exported data */
+var $form = document.querySelector('#entry-form');
+var $title = $form.elements.title;
+var $notes = $form.elements.notes;
+var $photoUrl = $form.elements.photoUrl;
+var $photo = document.querySelector('#photo');
+var n = 1;
 
-var $photoUrl = document.querySelector('#photo-url');
-var $img = document.querySelector('img');
-
-function updateSrc(event) {
+$photoUrl.addEventListener('input', function updateSrc(event) {
   var $currentUrl = $photoUrl.value;
-  $img.setAttribute('src', $currentUrl);
-}
+  $photo.setAttribute('src', $currentUrl);
+});
 
-$photoUrl.addEventListener('input', updateSrc);
+$form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  var entry = {};
+  entry.title = $form.elements.title.value;
+  entry.photoUrl = $form.elements.photoUrl.value;
+  entry.notes = $form.elements.notes.value;
+  entry.nextEntryID = n;
+  n += 1;
+  data.entries.push(entry);
+  $title.value = '';
+  $photoUrl.value = '';
+  $notes.value = '';
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+});
