d6c18c190d37837a694014ee667b0f784677c9ab
Christine Tina Park <escp1004jk@gmail.com>
2021-09-07 17:32:27 -0700

    Update src with url input.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photo-url');
+var $img = document.querySelector('img');
+
+function updateSrc(event) {
+  var $currentUrl = $photoUrl.value;
+  $img.setAttribute('src', $currentUrl);
+}
+
+$photoUrl.addEventListener('input', updateSrc);
